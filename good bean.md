f1c11984c95e08774a9bddb4ddf317f65dafc955
Becca <95663074+Acanthodoris@users.noreply.github.com>
2022-01-23 17:15:15 -0800

    finished homework

---
+++ good bean.md
@@ -0,0 +1,43 @@
+
+
+# good bean
+
+![GitHub license](https://img.shields.io/badge/License-Unlicense-yellow.svg)
+
+## Description
+
+* this is a good bean project
+
+### Table of Contents
+
+[Installation](#installation)
+
+[How to use](#How-to-use)
+
+[Licenses Used](#licenses)
+
+[Tests](#tests)
+
+[How to Contribute](#Contribute)
+
+[Report Issues](#Report-issues)
+
+
+## How-to-use:
+* open a can of beans
+
+## How-to-install:
+* eat the bean
+
+## Licenses-Used:
+* Unlicense
+
+## Tests:
+* trying a varitety of beans
+
+## How-to-Contribute-to-this-Project:
+* Nope
+
+## Report-issues:
+* [beans@notarealemail.com](mailto:beans@notarealemail.com)
+* [onlybeans](https://github.com/onlybeans)
---
+++ index.js
@@ -0,0 +1,117 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = () => {
+    return inquirer.prompt([{
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Give a short description of your project",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "How to install:",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "How to use the app:",
+            name: "howToUse"
+        },
+        {
+            type: "checkbox",
+            message: "What license were used?",
+            choices: ['Apache', 'Boost', 'MIT', 'Unlicense'],
+            name: "licenses"
+        },
+        {
+            type: "input",
+            message: "How do you test your project?",
+            name: "tests"
+        },
+        {
+            type: "input",
+            message: "Do you have any for guidlines to contributing to this projects?",
+            name: "contribute"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your GitHub?",
+            name: "github"
+        }
+    ]);
+};
+
+const generateReadme = ({
+    title,
+    description,
+    howToUse,
+    installation,
+    licenses,
+    tests,
+    contribute,
+    email,
+    github
+}) => `
+
+# ${title}
+
+![GitHub license](https://img.shields.io/badge/License-${licenses}-yellow.svg)
+
+## Description
+
+* ${description}
+
+### Table of Contents
+
+[Installation](#installation)
+
+[How to use](#How-to-use)
+
+[Licenses Used](#licenses)
+
+[Tests](#tests)
+
+[How to Contribute](#Contribute)
+
+[Report Issues](#Report-issues)
+
+
+## How-to-use:
+* ${howToUse}
+
+## How-to-install:
+* ${installation}
+
+## Licenses-Used:
+* ${licenses}
+
+## Tests:
+* ${tests}
+
+## How-to-Contribute-to-this-Project:
+* ${contribute}
+
+## Report-issues:
+* [${email}](mailto:${email})
+* [${github}](https://github.com/${github})
+`
+
+const init = () => {
+    questions()
+    .then((userInput) => fs.writeFileSync(`${userInput.title}.md`, generateReadme(userInput)))
+    .then(() => console.log('generated readme'))
+    .catch((err) => {
+        throw err
+    })
+}
+init();
